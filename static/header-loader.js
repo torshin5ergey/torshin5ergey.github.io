65726626f374bad05a1d8e9d048ef145d365fcd0
torshin5ergey <ttorshin5ergey@gmail.com>
2025-03-21 12:41:03 +0300

    create 1 file and update 1 file

--- static/header-loader.js
+++ static/header-loader.js
@@ -5,17 +5,17 @@ function loadHeader() {
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-container').innerHTML = data;
-        })
+        });
 }
 document.addEventListener('DOMContentLoaded', loadHeader);
 
 function openRandomArt() {
-    let pages = [
+    const pages = [
         '/cellular-automata/conways-simulator/index.html',
         '/cellular-automata/langtons-ant/index.html',
         '/cellular-automata/wolframs-rules/index.html',
         '/cellular-automata/maze-rule/index.html',
     ];
-    let randomPage = pages[Math.floor(Math.random() * pages.length)];
+    const randomPage = pages[Math.floor(Math.random() * pages.length)];
     window.location.href = randomPage;
 }
